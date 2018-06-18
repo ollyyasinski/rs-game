e61b968ac234e3174e3b941b369db96244749604
ollyysinski <you@example.com>
2018-06-18 20:33:32 +0300

    sync with latest version. minify heroes

--- js/modules/game-settings.js
+++ js/modules/game-settings.js
@@ -20,67 +20,96 @@ class SoundSlider {
         this.plusBtn = plusBtn;
         this.soundLevel;
     };
-    createSoundSlider() {
+    createSoundSlider(soundLevel) {
         lineHeight = ($(this.soundLine).height());
         let sliderSoundLine = this.soundLine,
             sliderSoundBtn = this.soundBtn,
             sliderMinusBtn = this.minusBtn,
             sliderPlusBtn = this.plusBtn;
 
-        $(sliderSoundBtn).draggable({
-            axis: 'y',
-            containment: 'parent'
-        });
+        let findSliderPosition = (soundLevel) => {
+            for (let i in SOUND_LEVELS) {
+                let soundObj = SOUND_LEVELS[i],
+                    sliderPosition = _.keys(soundObj)[0],
+                    soundLevelNumber = soundObj[sliderPosition];
 
-        $(sliderSoundBtn).on('drag', () => {
-            let soundLevel = $(sliderSoundBtn).position().top;
-
-            $(sliderSoundLine).css({
-                'clip': 'rect(' + soundLevel + CLIP_PXS
-            });
-        });
-
-
-        $(sliderMinusBtn).on('click', function () {
-            let soundLevel = $(sliderSoundBtn).position().top + MOVE_LENGTH;
-
-            $(sliderSoundLine).css({
-                'clip': 'rect(' + soundLevel + CLIP_PXS
-            });
-
-            if (soundLevel <= lineHeight - (MOVE_LENGTH / 2)) {
-                $(sliderSoundBtn).css({
-                    'top': soundLevel
-                });
-            } else {
-                soundLevel = lineHeight - (MOVE_LENGTH / 2);
-            }
-        });
-
-        $(sliderPlusBtn).on('click', function () {
-            let soundLevel = $(sliderSoundBtn).position().top - MOVE_LENGTH;
-
-            $(sliderSoundLine).css({
-                'clip': 'rect(' + soundLevel + CLIP_PXS
-            });
-
-            if (soundLevel + MOVE_LENGTH > 0) {
-                $(sliderSoundBtn).css({
-                    'top': soundLevel
-                });
-            };
-
-            if (soundLevel >= lineHeight - (MOVE_LENGTH / 2)) {
-                soundLevel = lineHeight - (MOVE_LENGTH * 2);
+                if (soundLevel === soundLevelNumber) {
+                    return sliderPosition;
+                }
             }
         }
-        );
 
+        let setSliderToCurrentPosition = () => {
+            $(sliderSoundLine).css({
+                'clip': 'rect(' + findSliderPosition(soundLevel) + CLIP_PXS
+            });
+            $(sliderSoundBtn).css({
+                'top': findSliderPosition(soundLevel) - (MOVE_LENGTH / 2)
+            });
+            $(sliderSoundBtn).draggable({
+                axis: 'y',
+                containment: 'parent'
+            });
+        }
+
+        let reactToDrag = () => {
+            $(sliderSoundBtn).on('drag', () => {
+                let soundLevel = $(sliderSoundBtn).position().top;
+
+                $(sliderSoundLine).css({
+                    'clip': 'rect(' + soundLevel + CLIP_PXS
+                });
+            });
+        }
+
+        let reactToMinusBtn = () => {
+            $(sliderMinusBtn).on('click', function () {
+                let soundLevel = $(sliderSoundBtn).position().top + MOVE_LENGTH;
+
+                $(sliderSoundLine).css({
+                    'clip': 'rect(' + soundLevel + CLIP_PXS
+                });
+
+                if (soundLevel <= lineHeight - (MOVE_LENGTH / 2)) {
+                    $(sliderSoundBtn).css({
+                        'top': soundLevel
+                    });
+                } else {
+                    soundLevel = lineHeight - (MOVE_LENGTH / 2);
+                }
+            });
+        }
+
+        let reactToPlusBtn = () => {
+            $(sliderPlusBtn).on('click', function () {
+                let soundLevel = $(sliderSoundBtn).position().top - MOVE_LENGTH;
+
+                $(sliderSoundLine).css({
+                    'clip': 'rect(' + soundLevel + CLIP_PXS
+                });
+
+                if (soundLevel + MOVE_LENGTH > 0) {
+                    $(sliderSoundBtn).css({
+                        'top': soundLevel
+                    });
+                };
+
+                if (soundLevel >= lineHeight - (MOVE_LENGTH / 2)) {
+                    soundLevel = lineHeight - (MOVE_LENGTH * 2);
+                }
+            }
+            );
+        }
+
+        setSliderToCurrentPosition();
+        reactToDrag();
+        reactToMinusBtn();
+        reactToPlusBtn();
     }
     getSoundSetting(sliderSoundBtn) {
         for (let i in SOUND_LEVELS) {
             soundLevel = new Helpers().roundToTwenty($(sliderSoundBtn).position().top, MOVE_LENGTH, 0);
-            let soundLevelNumber = Number(Object.keys(SOUND_LEVELS[i]));
+            let soundLevelNumber = Number(_.keys(SOUND_LEVELS[i]));
             if (soundLevel === soundLevelNumber) {
                 return SOUND_LEVELS[i][soundLevelNumber];
             }
@@ -91,70 +120,93 @@ class SoundSlider {
 class SideNav {
     constructor() { };
     createSideNav(level, levelLanguage) {
-        $(".game-background").append(SIDE_NAV_HTML);
 
-        $("#humbergerBtn").click(() => {
-            $(".background-opacity-wrapper").addClass("background-opacity-wrapper-width");
-            $(".sidenav").addClass("sidenav-width");
-        });
+        let createSideNav = () => {
+            $(".game-background").append(SIDE_NAV_HTML);
 
-        $("#closeBtn").click(() => {
-            $(".background-opacity-wrapper").removeClass("background-opacity-wrapper-width");
-            $(".sidenav").removeClass("sidenav-width");
-        });
+            $("#humbergerBtn").click(() => {
+                $(".background-opacity-wrapper").addClass("background-opacity-wrapper-width");
+                $(".sidenav").addClass("sidenav-width");
+            });
 
-        $('#officeColors').click(() => {
-            this.showOfficeSelector();
-        })
+            $("#closeBtn").click(() => {
+                $(".background-opacity-wrapper").removeClass("background-opacity-wrapper-width");
+                $(".sidenav").removeClass("sidenav-width");
+            });
+        }
 
-        $('#soundSettings').click(() => {
-            this.showSoundSelector();
-        });
+        let createSideNavMenuItems = () => {
+            $('#officeColors').click(() => {
+                this.showOfficeSelector();
+            })
 
-        $("#bestResults").click(() => {
-            this.showResults();
-        });
-        $('#rules').click(() => {
-            this.showRules();
-        });
+            $('#soundSettings').click(() => {
+                this.showSoundSelector();
+            });
+
+            $("#bestResults").click(() => {
+                this.showResults();
+            });
+            $('#rules').click(() => {
+                this.showRules();
+            });
+        }
+
+        createSideNav();
+        createSideNavMenuItems();
     }
     showOfficeSelector() {
-        $(".game-background").append(OFFICE_SETTINGS_HTML);
-        let officesArray = $(".office-option").toArray();
-        for (let i in officesArray) {
-            $(officesArray[i]).css('background-image', `url("assets/img/office-background/${OFFICE_COLORS[i]}-offices/${selectedOffice}.png")`);
-            $(officesArray[i]).click(new Helpers().selectElement);
+        let createOfficeSettingsModal = () => {
+            $(".game-background").append(OFFICE_SETTINGS_HTML);
+            let officesArray = $(".office-option").toArray();
+            for (let i in officesArray) {
+                $(officesArray[i]).css('background-image', `url("assets/img/office-background/${OFFICE_COLORS[i]}-offices/${selectedOffice}.png")`);
+                $(officesArray[i]).click(new Helpers().selectElement);
+            }
+            this.closeMenuModal("#closeOffices");
         }
 
-        $("#saveOfficeBtn").click(() => {
-            let selectedBgd = $(".selected").css("background-image");
-            gameColor = selectedBgd.match("(?<=background\/)(.*)(?=-offices)")[0];
-            gameBackground.css("background-image", selectedBgd);
-            $(".menu-modal").remove();
-            $(".background-opacity-wrapper").removeClass("background-opacity-wrapper-width");
-            $(".sidenav").removeClass("sidenav-width");
+        let applySelectedOffice = () => {
+            $("#saveOfficeBtn").click(() => {
+                let selectedBgd = $(".selected").css("background-image");
+                gameColor = selectedBgd.match("(?<=background\/)(.*)(?=-offices)")[0];
+                gameBackground.css("background-image", selectedBgd);
+                $(".menu-modal").remove();
+                $(".background-opacity-wrapper").removeClass("background-opacity-wrapper-width");
+                $(".sidenav").removeClass("sidenav-width");
+            }
+            )
         }
-        )
 
-        this.closeMenuModal("#closeOffices");
+        createOfficeSettingsModal();
+        applySelectedOffice();
     }
     showSoundSelector() {
-        $(".game-background").append(SOUND_SETTINGS_HTML);
-
         let volumeSlider = new SoundSlider("#volumeLine", "#volumeBtn", "#volumeMinusBtn", "#volumePlusBtn");
         let speedSlider = new SoundSlider("#speedLine", "#speedBtn", "#speedMinusBtn", "#speedPlusBtn");
-        volumeSlider.createSoundSlider();
-        speedSlider.createSoundSlider();
 
-        $("#saveSoundBtn").click(() => {
-            volume = volumeSlider.getSoundSetting("#volumeBtn");
-            rate = speedSlider.getSoundSetting("#speedBtn");
-            $(".menu-modal").remove();
-            $(".background-opacity-wrapper").removeClass("background-opacity-wrapper-width");
-            $(".sidenav").removeClass("sidenav-width");
-        });
+        let createSoundSelectorModule = () => {
+            $(".game-background").append(SOUND_SETTINGS_HTML);
 
-        this.closeMenuModal("#closeSound");
+            volumeSlider.createSoundSlider(volume);
+            speedSlider.createSoundSlider(rate);
+
+            this.closeMenuModal("#closeSound");
+        };
+
+        let applySoundSettings = () => {
+            $("#saveSoundBtn").click(() => {
+                volume = volumeSlider.getSoundSetting("#volumeBtn");
+                rate = speedSlider.getSoundSetting("#speedBtn");
+                $(".menu-modal").remove();
+                $(".background-opacity-wrapper").removeClass("background-opacity-wrapper-width");
+                $(".sidenav").removeClass("sidenav-width");
+            });
+        };
+
+        createSoundSelectorModule();
+        applySoundSettings();
+
     }
     showRules() {
         $(".game-background").append(RULES_HTML);
